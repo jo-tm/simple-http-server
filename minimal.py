9dadae956ca7f16f6ed71e658251335561e86898
joigno <30682875+joigno@users.noreply.github.com>
2022-09-07 18:03:04 +0200

    port 8000

--- minimal.py
+++ minimal.py
@@ -75,7 +75,7 @@ class S(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps(response).encode('utf-8'))
         #self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
-def run(server_class=HTTPServer, handler_class=S, port=8080):
+def run(server_class=HTTPServer, handler_class=S, port=8000):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
