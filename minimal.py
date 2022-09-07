2a532aebe24e8dcba7be03141ed558a5a7cfa28a
joigno <30682875+joigno@users.noreply.github.com>
2022-09-08 00:11:44 +0200

    Update minimal.py

--- minimal.py
+++ minimal.py
@@ -7,6 +7,9 @@ Usage::
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging, json
 
+import json
+from web3 import Web3
+
 ## Strategy api-spot example request
 example_req = '''
 {
@@ -47,10 +50,66 @@ response = {
   "score": []
 }
 
+mass_delegate_req = '''
+{
+  "network": "1",
+  "delegator": "0xdededededededededededededededededededede"
+  "delegatees": [
+        {
+            "address" : "0xdededededededededededededededededededede",
+            "weight" : "95"
+        },
+        {
+            "address" : "0xd649bACfF66f1C85618c5376ee4F38e43eE53b63",
+            "weight" : "1"
+        },
+        {
+            "address" : "0xEA2E9cEcDFF8bbfF107a349aDB9Ad0bd7b08a7B7",
+            "weight" : "1"
+        },
+        {
+            "address" : "0x3c4B8C52Ed4c29eE402D9c91FfAe1Db2BAdd228D",
+            "weight" : "1"
+        },
+        {
+            "address" : "0x726022a9fe1322fA9590FB244b8164936bB00489",
+            "weight" : "1"
+        },
+        {
+            "address" : "0xc6665eb39d2106fb1DBE54bf19190F82FD535c19",
+            "weight" : "1"
+        }
+  ],
+  "snapshot": 11437846
+}
+'''
+
+def balance_of_erc20(addr):
+    myabi = '''[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"},{"name":"_releaseTime","type":"uint256"}],"name":"mintTimelocked","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]'''
+    infura_url = "https://rinkeby.infura.io/v3/890b63f3265f4331ae435bef1c0869b8"
+    web3 = Web3(Web3.HTTPProvider(infura_url))
+    abi=json.loads(myabi)
+    address = "0xa63C6E2Af9c87768f741E1BB7f77511d23152C7B"
+    contract = web3.eth.contract(address=address, abi=abi)
+    totalSupply = contract.functions.totalSupply().call()
+    #print(totalSupply)
+    #print(contract.functions.name().call())
+    #print(contract.functions.symbol().call())
+    address2 = Web3.toChecksumAddress(addr)
+    balance = contract.functions.balanceOf(address2).call()
+    #print(balance)
+    int_balance = (balance) // 10**18
+    #print(int_balance)
+    return str(int_balance)
+
 def voting_power(addr):
     return str(1)
 
 class S(BaseHTTPRequestHandler):
+
+    _delegators = {}
+    _delegatees = {}
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -75,6 +134,8 @@ class S(BaseHTTPRequestHandler):
             response["score"] = []
             for addr in obj['addresses']:
                 response["score"].append( {"address" : addr, "score" : voting_power(addr)} )
+        elif self.path == "/mass-delegate":
+            pass
 
         self._set_response()
         self.wfile.write(json.dumps(response, indent=4).encode('utf-8'))
